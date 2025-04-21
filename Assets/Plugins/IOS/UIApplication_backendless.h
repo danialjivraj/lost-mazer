778fcac0865a1a69e706a70572889dcbbe3cedf1
Danial Jivraj <danialjivraj@gmail.com>
2025-04-21 16:27:28 +0100

    cleaning unused assets

--- Assets/Plugins/IOS/UIApplication_backendless.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface UIApplication (SupressWarnings)
-- (void)application:(UIApplication *)application backendlessDidRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken;
-- (void)application:(UIApplication *)application backendlessDidFailToRegisterForRemoteNotificationsWithError:(NSError *)err;
-- (void)application:(UIApplication *)application backendlessDidReceiveRemoteNotification:(NSDictionary *)userInfo;
-- (void)application:(UIApplication *)application backendlessDidReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))handler;
-- (BOOL)application:(UIApplication *)application backendlessDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
-@end
