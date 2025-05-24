69677426cbf92e3fd069f1e44156ec73304a36f2
Danial Jivraj <danialjivraj@gmail.com>
2025-05-25 00:24:19 +0100

    fixing gitignore

--- Assets/_Scripts/BackendlessConfig.cs
+++ Assets/_Scripts/BackendlessConfig.cs
@@ -2,9 +2,9 @@ using UnityEngine.Networking;
 
 public static class BackendlessConfig
 {
-    public const string BASE_URL = "https://zootychicken-us.backendless.app";
-    public const string APP_ID = "70CF2EE3-D68E-4464-8689-9C5E7391BF1E";
-    public const string REST_API_KEY = "126FC3D3-87F5-47C9-9C99-811BE01A1622";
+    public const string BASE_URL = "";
+    public const string APP_ID = "";
+    public const string REST_API_KEY = "";
 
     public const string TABLE_NAME = "HighScores";
 
